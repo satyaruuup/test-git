d3e5edcdddee6ab3b08978213bc2a4199f15a2ee
Umesh Kumar <satyaruuup@gmail.com>
2020-01-21 20:17:21 +0530

    test 1

--- CoreR/CoreR/Program.cs
+++ CoreR/CoreR/Program.cs
@@ -15,7 +15,7 @@ namespace CoreR
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-        }
+        } 
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
